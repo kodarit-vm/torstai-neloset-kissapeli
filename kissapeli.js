8fde6f08a678ff0c9e18fd0302ab69017d6b1909
Vekkumasa <ylirasku@gmail.com>
2021-03-03 10:28:41 +0200

    jepjep

---
+++ kissapeli.js
@@ -0,0 +1,54 @@
+let taustakuva;
+let kissakuva;
+let kissa;
+
+let taustan_korkeus = 400;
+let taustan_leveys = 800;
+let lautan_leveys = 50;
+let lautan_korkeus = 20;
+
+function preload() {
+  taustakuva = loadImage('https://igno.cc/opetus/kuvat/tausta.png');
+  kissakuva = loadImage('https://igno.cc/opetus/kuvat/cat.png');
+}
+
+function setup() {
+  kissa = new Kissa();
+  var canvas = createCanvas(taustan_leveys, taustan_korkeus);
+}
+
+function draw() {
+  image(taustakuva, 0, 0, taustan_leveys, taustan_korkeus);
+  luo_lautta();
+  kissa.liikuta();
+}
+
+function luo_lautta() {
+  fill('rgba(0, 255, 0, 0.25)'); // Red Green Blue 0 - 255 hexadesimaali arvot = 0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F
+  if (mouseX > taustan_leveys) {
+    rect(taustan_leveys - 20, taustan_korkeus -50, lautan_leveys, lautan_korkeus, 30, 30, 10, 10);
+  } else {
+    rect(mouseX, taustan_korkeus -50, lautan_leveys, lautan_korkeus, 30, 30, 10, 10);
+  }
+}
+
+class Kissa {
+  constructor() {
+    this.X = 30;
+    this.Y = 200;
+    this.korkeus = 50;
+    this.leveys = 50;
+    this.xNopeus = 2;
+    this.yNopeus = -2;
+  }
+
+  liikuta() {
+    image(kissakuva, this.X, this.Y, this.leveys, this.korkeus);
+
+//    this.X = this.X + this.xNopeus;
+    this.X += this.xNopeus;
+    this.yNopeus += 0.05;
+    this.Y += this.yNopeus;
+//    console.log(this.Y); ctrl + shift + i TAI f12 avaa konsolin chromessa.
+  }
+}
